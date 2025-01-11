4a40bb4391712a361bfced07acda6218a13088d8
Surya <suryaarioprrama@gmail.com>
2025-01-11 19:01:12 +0700

    adding footer to component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,26 @@
+ <footer>
+        <div class="footer layout-light  p-0  py-5 lay_25 d-flex justify-content-center flex-column">
+            <div class="  d-flex justify-content-center ">
+                <div class="dark-text mx-4">
+                    <h3>Warnet Digital Hub</h3>
+                    <p>Lorem ipsum dolor sit amet <br>consectetur adipisicing elit. <br>Quisquam, quod.</p>
+                </div>
+                <div class="dark-text mx-4">
+                    <h3>Services</h3>
+                    <ul>
+                        <li>Play Game</li>
+                        <li>Print Document</li>
+                        <li>Internet</li>
+                    </ul>
+                </div>
+                <div class="dark-text mx-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Instagram</li>
+                        <li>Twitter</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -153,7 +153,7 @@
         {{-- </div> --}}
         {{-- end card game --}}
 
-        <div class="my-5">
+        <div class="my-5 lay_50 d-flex justify-content-center flex-column">
             <h1 class="text-center mb-5 light-text">Our Games</h1>
             {{-- Jika tidak ada game, tampilkan pesan --}}
             @if ($games->isEmpty())
@@ -205,30 +205,5 @@
 @endsection
 {{-- end Body --}}
 @section('footer')
-    <footer>
-        <div class="footer layout-light  p-0  py-5">
-            <div class="  d-flex justify-content-center ">
-                <div class="dark-text mx-4">
-                    <h3>Warnet Digital Hub</h3>
-                    <p>Lorem ipsum dolor sit amet <br>consectetur adipisicing elit. <br>Quisquam, quod.</p>
-                </div>
-                <div class="dark-text mx-4">
-                    <h3>Services</h3>
-                    <ul>
-                        <li>Play Game</li>
-                        <li>Print Document</li>
-                        <li>Internet</li>
-                    </ul>
-                </div>
-                <div class="dark-text mx-4">
-                    <h3>Follow Us</h3>
-                    <ul>
-                        <li>Facebook</li>
-                        <li>Instagram</li>
-                        <li>Twitter</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+   <x-footer />
 @endsection
