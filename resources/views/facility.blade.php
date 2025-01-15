7a4400d47b04ce06202d59d909c35f1585bbc1a1
Surya <suryaarioprrama@gmail.com>
2025-01-15 14:22:10 +0700

    fixing layout and text facilites

--- resources/views/facility.blade.php
+++ resources/views/facility.blade.php
@@ -14,20 +14,15 @@
         }
 
         .ul_right {
-            direction: rtl;
-            /* Mengatur arah elemen ke kanan */
-            list-style-position: inside;
-            /* Bullet di dalam area elemen */
             text-align: right;
-            /* Menyelaraskan teks ke kanan */
+            list-style-position: inside;
         }
 
-        .li_cost_right::before {
+        .li_cost_right::after {
             content: "✅";
-            /* Emoji pengganti */
             margin-left: 10px;
-            /* Jarak antara teks dan emoji */
         }
+
 
         .li_cost::before {
             content: "✅";
@@ -41,7 +36,7 @@
         {{-- 1 --}}
         <div>
             <h1 class="text-center my-5">PS 5, 4, 3 & Console</h1>
-            <div class="d-flex justify-content-center gap-5">
+            <div class="d-flex justify-content-center align-items-center  gap-5">
                 <div>
                     <span class="fs-5">
                         <strong>Variety of console and game</strong>
@@ -70,27 +65,28 @@
 
         {{-- 2 --}}
         <div class="">
-            <h1 class="text-center my-5">PS 5, 4, 3 & Console</h1>
-            <div class="d-flex justify-content-center gap-5">
+            <h1 class="text-center my-5">PC</h1>
+            <div class="d-flex justify-content-center align-items-center gap-5">
                 <div>
                     <img src="{{ asset('img/ps5.png') }}" alt="">
                 </div>
 
-                <div>
-                    <span class="fs-5">
-                        <strong>Variety of console and game</strong>
+                <div class="text-end">
+                    <span class="fs-5 ">
+                        <strong class="">Enjoy playing casual or AAA games!</strong>
                     </span>
                     <br>
                     <span class="fs-7">
                         {{-- ul>li.licost*3 --}}
-                        <ul class="ul_right">
+                        <ul class="ul_right text-end">
                             <li class="li_cost_right">Available Consoles: We provide PS3, PS4, and PS5 for an amazing gaming
                                 experience.
                             </li>
                             <li class="li_cost_right">Choose Your Game: Enjoy a wide selection of games across all console
                                 generations.
                             </li>
-                            <li class="li_cost_right">Order your console 15 minutes before you arrives!</li>
+                            <li class="li_cost_right">Order your console <strong>15 minutes</strong> before you arrives!
+                            </li>
 
                         </ul>
                     </span>
@@ -98,63 +94,62 @@
             </div>
         </div>
         {{-- 3 --}}
-        <div>
-            <h1 class="text-center my-5">PS 5, 4, 3 & Console</h1>
-            <div class="d-flex justify-content-center gap-5">
+        <div class="">
+            <h1 class="text-center my-5">Canteen</h1>
+            <div class="d-flex justify-content-center align-items-center gap-5">
                 <div>
                     <span class="fs-5">
-                        <strong>Variety of console and game</strong>
+                        <strong>Order Your Favorite Food and Drinks with Ease</strong>
                     </span>
                     <br>
                     <span class="fs-7">
                         {{-- ul>li.licost*3 --}}
                         <ul class="">
-                            <li class="li_cost">Available Consoles: We provide PS3, PS4, and PS5 for an amazing gaming
-                                experience.
+                            <li class="li_cost">Browse our delicious menu right from store app at your pc.
                             </li>
-                            <li class="li_cost">Choose Your Game: Enjoy a wide selection of games across all console
-                                generations.
+                            <li class="li_cost">Buy with m-banking, qris, or cash.
                             </li>
-                            <li class="li_cost">Order your console 15 minutes before you arrives!</li>
-
+                            <li class="li_cost">Relax and enjoy gaming while your food delivered to your place.</li>
                         </ul>
                     </span>
-                </div>
 
+                </div>
                 <div>
                     <img src="{{ asset('img/ps5.png') }}" alt="">
                 </div>
             </div>
         </div>
         {{-- 4 --}}
-        <div class="mb-5">
-            <h1 class="text-center my-5">PS 5, 4, 3 & Console</h1>
-            <div class="d-flex justify-content-center gap-5">
+        <div class=" mb-5">
+            <h1 class="text-center my-5">Open 24/7</h1>
+            <div class="d-flex justify-content-center align-items-center gap-5">
                 <div>
                     <img src="{{ asset('img/ps5.png') }}" alt="">
                 </div>
-
-                <div>
-                    <span class="fs-5">
-                        <strong>Variety of console and game</strong>
+                <div class="text-end">
+                    <span class="fs-5 ">
+                        <strong>Enjoy playing whenever you want!</strong>
                     </span>
                     <br>
                     <span class="fs-7">
                         {{-- ul>li.licost*3 --}}
                         <ul class="ul_right">
-                            <li class="li_cost_right">Available Consoles: We provide PS3, PS4, and PS5 for an amazing gaming
-                                experience.
+                            <li class="li_cost_right">Come anytime, no need to worry about holidays—we're always open!
                             </li>
-                            <li class="li_cost_right">Choose Your Game: Enjoy a wide selection of games across all console
-                                generations.
+                            <li class="li_cost_right">Our security guards are on duty to ensure a safe and secure experience
                             </li>
-                            <li class="li_cost_right">Order your console 15 minutes before you arrives!</li>
+                            <li class="li_cost_right">Enjoy the convenience of ordering and relaxing in a secure environment,
+                                anytime you like!</li>
 
                         </ul>
                     </span>
                 </div>
+
+
             </div>
         </div>
+        {{-- 4 --}}
+
     </main>
 @endsection
 @section('footer')
